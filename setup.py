13d969358add9b8020fc8f9cbe93b04a1c56e8dc
Jamie Titus <ctitus@stanford.edu>
2021-09-29 12:32:33 -0400

    Committing most of Eliot's code from SSTObjects, repackaged as sst_hw

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().split()
+
+setup(
+    author="NIST Staff",
+    author_email=None,
+    description="SST hardware definitions",
+    install_requires=requirements,
+    name="sst_hw",
+    use_scm_version=True,
+    packages=find_packages()
+)
