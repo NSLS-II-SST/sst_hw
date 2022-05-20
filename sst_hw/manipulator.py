60d0f5e61b75bd7d4e1974694383318b033193e7
Charles Titus <ctitus@bnl.gov>
2022-05-20 17:41:44 -0400

    updates to multimesh

--- sst_hw/manipulator.py
+++ sst_hw/manipulator.py
@@ -1,10 +1,10 @@
 from sst_base.manipulator import Manipulator1AxBase
-from sst_base.motors import PrettyMotorFMBO
+from sst_base.motors import PrettyMotor
 from ophyd import Component as Cpt
 
 
 class MultiMesh(Manipulator1AxBase):
-    x = Cpt(PrettyMotorFMBO, "Mmesh}Mtr", name="Multimesh")
+    x = Cpt(PrettyMotor, "MMesh}Mtr", name="Multimesh")
 
 
 multimesh = MultiMesh(None, "XF:07ID1-BI{I0Up-Ax:", name="i0upmultimesh")
